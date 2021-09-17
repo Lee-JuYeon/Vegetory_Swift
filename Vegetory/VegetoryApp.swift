208395a3c0bd4aae61e6d654ac2c18aeb5353725
JUYEON_LEE <cavss1118@gmail.com>
2021-09-17 17:46:08 +0900

    Initial Commit

---
+++ Vegetory/VegetoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VegetoryApp.swift
+//  Vegetory
+//
+//  Created by 이주연 on 2021/09/17.
+//
+
+import SwiftUI
+
+@main
+struct VegetoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

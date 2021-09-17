0b01877d2f2c29bc2a9606b1ffd2312c63a2fa52
JUYEON_LEE <cavss1118@gmail.com>
2021-09-17 18:31:50 +0900

    #00001 TopbarModel. 확장자는 identifiable로 해줘서 나중에 listview에 foreach가 가능하도록 했다. identifiable은 var id = UUID()를 필수적으로 해주어 foreach에 id설정을 따로 하지 않아도 되는 원리다.

---
+++ Vegetory/Model/TopBar/TopBarModel.swift
@@ -0,0 +1,14 @@
+//
+//  TopBarModel.swift
+//  Vegetory
+//
+//  Created by 이주연 on 2021/09/17.
+//
+
+import SwiftUI
+
+struct TopBarModel : Identifiable {
+    var id = UUID()
+    var titleNativ : String
+    var titleEng : String
+}

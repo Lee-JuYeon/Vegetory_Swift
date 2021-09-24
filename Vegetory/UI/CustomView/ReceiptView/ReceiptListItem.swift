8de0e99d6880a4e9f2a5b898412a5e44e6a31759
JuYeon Lee <cavss1118@gmail.com>
2021-09-24 21:55:17 +0900

    #34 Receipt productList item View. // 1. 이부분을 클릭하면 클릭 리스너가 invoke되지 않는데, 그 이유는 아마 클릭 리스너를 설정하지 않아서 그런 듯 싶다.

---
+++ Vegetory/UI/CustomView/ReceiptView/ReceiptListItem.swift
@@ -0,0 +1,45 @@
+//
+//  ReceiptListItem.swift
+//  Vegetory
+//
+//  Created by 이주연 on 2021/09/24.
+//
+
+import SwiftUI
+
+struct ReceiptListItem: View {
+    var productImage : String
+    var productName : String
+    var productPrice : Int
+    var count : Int
+    
+    var body: some View {
+        HStack(alignment : VerticalAlignment.center){
+            Text("\(productName) x\(count)")
+                .font(.custom("NotoSansKR-Bold", size: 13))
+                .frame(
+                    alignment:.leading
+                )
+            
+            Text("\(productPrice)원")
+                .font(.custom("NotoSansKR-Medium", size: 13))
+                .frame(
+                    minWidth: 0,
+                    maxWidth: .infinity,
+                    minHeight: 0,
+                    idealHeight: 18,
+                    maxHeight: 18,
+                    alignment: Alignment.trailing
+                )
+        }
+        .frame(
+            minWidth: 0,
+            idealWidth: 50,
+            maxWidth: .infinity,
+            minHeight: 0,
+            idealHeight: 18,
+            maxHeight: 18,
+            alignment: Alignment.leading
+        )
+    }
+}

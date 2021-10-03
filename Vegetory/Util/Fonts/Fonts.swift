f0b9734065bf8028a4579df1aaea5fe18ec2179c
JuYeon Lee <cavss1118@gmail.com>
2021-10-03 20:23:30 +0900

    #54 Font setting

---
+++ Vegetory/Util/Fonts/Fonts.swift
@@ -0,0 +1,17 @@
+//
+//  Fonts.swift
+//  Vegetory
+//
+//  Created by 이주연 on 2021/09/30.
+//
+
+import SwiftUI
+
+enum FontType : String {
+    case Eulyoo_Regular = "Eulyoo1945-Regular"
+    case Eulyoo_SamiBold = "Eulyoo1945-SemiBold"
+    case ImcreSoojin = "ImcreSoojin OTF"
+    case NotoSans_Bold = "NotoSansKR-Bold"
+    case NotoSans_Medium = "NotoSansKR-Medium"
+    case KOTRA = "KOTRA_BOLD"
+}
